fdcd4cc3fb79cbd3113904a2a406926d45c583fe
samoh <samoh@2111DR000995>
2023-10-09 15:19:42 +0200

    Revert "Revert "feat!: project setup, added common properties and webdriver setup""
    This reverts commit cd235ad5ff9702db60a618f54b40ea7154fa82e7.

---
+++ opnsenseTests/src/test/java/common/DriverSetupAndQuit.java
@@ -0,0 +1,41 @@
+package common;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverSetupAndQuit {
+public static WebDriver driver = null;
+	
+	public String login, password, url;
+	
+	@BeforeSuite
+	public void launchBrowser () {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();	
+	}
+	
+	@AfterSuite
+	public void closingBrowser() {
+		driver.quit();
+	}
+	
+	@BeforeSuite
+	public void getProperties() throws IOException {
+		FileInputStream fis = new FileInputStream(
+				System.getProperty("user.dir") + "\\src\\test\\java\\properties\\TestData.properties");
+		Properties p = new Properties();
+		p.load(fis);
+		login = p.getProperty("login");
+		password = p.getProperty("password");
+		url = p.getProperty("url");
+	}
+	
+	}
