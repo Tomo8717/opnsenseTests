9243212bebd2883f71a228e589f388b3f07068f5
Tomo8717 <Steph.Valm@gmail.com>
2023-10-10 09:55:56 +0200

    Changes in Logout test code structure
    fix: changed hardcoded wait length to fluid wait until element is displayed

--- opnsenseTests/src/test/java/tests/Logout.java
+++ opnsenseTests/src/test/java/tests/Logout.java
@@ -1,9 +1,12 @@
 package tests;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class Logout extends Login{
@@ -11,13 +14,10 @@ public class Logout extends Login{
 	@Test(groups="smoke")
 	public void logout() {
 		WebElement logout = driver.findElement(By.xpath("/html/body/main/aside/div/nav/div/div/div[1]/a[4]/div/div/div[2]/span"));
-
-		// wait until element is displayed
-				try {
-					TimeUnit.SECONDS.sleep(1);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOf(logout)).isDisplayed();
+		
 		logout.click();
 		
 		// for debug
