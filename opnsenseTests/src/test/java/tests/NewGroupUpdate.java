bc1effc966988ae9ad746e49e1192db0d1f92d0d
Tomo8717 <Steph.Valm@gmail.com>
2023-10-12 17:39:35 +0200

    Addition of 1 new test NewGroupUpdate
    feat: added test for editing newly made Group and checking that changes took place

---
+++ opnsenseTests/src/test/java/tests/NewGroupUpdate.java
@@ -0,0 +1,34 @@
+package tests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import common.LoginAction;
+
+public class NewGroupUpdate extends LoginAction {
+	@Test(groups={"regression","group_crud"}, priority = 2)
+	public void newGroupUpdate() {
+		driver.get("https://192.168.23.128/system_groupmanager.php");
+		//going to group manager
+		driver.findElement(By.xpath("//*[@href=\"system_groupmanager.php?act=edit&groupid=1\"]")).click();
+		//and clicking on edit button for our newly created group
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.name("name")))).isDisplayed();
+		
+		driver.findElement(By.xpath("//*[@name=\"description\"] [@type=\"text\"]")).clear();
+		driver.findElement(By.xpath("//*[@name=\"description\"] [@type=\"text\"]")).sendKeys("Changed description");
+		driver.findElement(By.id("save")).click();
+		// changing description, saving changes, getting out of edit group form 
+		
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[@data-groupid=\"1\"]")))).isDisplayed();
+		Assert.assertEquals(driver.getPageSource().contains("TestGroup")&driver.getPageSource().contains("Changed description"), true);
+		// checking that our changes took effect on group manager page.
+		
+	}
+}
