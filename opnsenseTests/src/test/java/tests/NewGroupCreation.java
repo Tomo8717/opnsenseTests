11ce444a33e4611d0395dedc4502c66aa3c0e0b3
Tomo8717 <Steph.Valm@gmail.com>
2023-10-12 16:28:08 +0200

    Addition of 2 tests for Groups: NewGroupCreation & NewGroupDeletion
    feat: added test for creation of a new UserGroup and check if it's
    created successfully
    
    feat: added test for deletion of a new UserGroup and check if it's deleted successfully

---
+++ opnsenseTests/src/test/java/tests/NewGroupCreation.java
@@ -0,0 +1,34 @@
+package tests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import common.LoginAction;
+
+public class NewGroupCreation extends LoginAction {
+	@Test(groups={"regression","group_crud"}, priority = 1)
+	public void newGroupCreation() {
+		driver.get("https://192.168.23.128/system_groupmanager.php");
+		//going to groups page 
+		
+		driver.findElement(By.xpath("//*[@data-original-title=\"Add\"]")).click();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.name("name")))).isDisplayed();
+		
+		driver.findElement(By.xpath("//*[@name=\"name\"]")).sendKeys("TestGroup");
+		driver.findElement(By.xpath("//*[@name=\"description\"] [@type=\"text\"]")).sendKeys("Description of TestGroup");
+		
+		driver.findElement(By.id("save")).click();
+		//creating and saving group w/ parameters
+		
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[@data-groupid=\"1\"]")))).isDisplayed();
+		Assert.assertEquals(driver.getPageSource().contains("TestGroup")&driver.getPageSource().contains("Description of TestGroup"), true);
+		
+	}
+}
---
+++ opnsenseTests/src/test/java/tests/NewGroupDeletion.java
@@ -0,0 +1,33 @@
+package tests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import common.LoginAction;
+
+public class NewGroupDeletion extends LoginAction {
+	@Test(groups={"regression","group_crud"}, priority = 3)
+	public void newGroupDeletion() {
+		driver.get("https://192.168.23.128/system_groupmanager.php");
+		//going to group manager
+		driver.findElement(By.xpath("//*[@data-groupname=\"TestGroup\"]")).click();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("modal-open")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class=\"bootstrap-dialog-footer-buttons\"]/button[2]")));
+        
+        driver.findElement(By.xpath("//*[@class=\"bootstrap-dialog-footer-buttons\"]/button[2]")).click();
+        //in modal dialog clicking the yes - it's always the second button hence the code
+        
+        driver.navigate().refresh();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"iform2\"]")));
+		
+		Assert.assertEquals(driver.getPageSource().contains("TestGroup"), false);
+		//waiting window reload and checking that we really deleted new group
+	}
+}
