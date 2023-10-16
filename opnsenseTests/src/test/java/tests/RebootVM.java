96c66f8f85c01be8b782cf3ce84fecfdbb6d11aa
Tomo8717 <Steph.Valm@gmail.com>
2023-10-16 12:01:26 +0200

    Addition 1 new test RebootVM
    feat: added new test to check if reboot button from GUI does indeed reboot.

---
+++ opnsenseTests/src/test/java/tests/RebootVM.java
@@ -0,0 +1,25 @@
+package tests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import common.LoginAction;
+
+public class RebootVM extends LoginAction {
+	@Test(groups="regression")
+	public void rebootVM() {
+		driver.get("https://192.168.23.128/ui/core/reboot");
+		//going to big red button page
+		
+		driver.findElement(By.id("do-reboot")).click();
+		//pushing big red button
+			
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"totals\"]")));
+		wait.until(ExpectedConditions.urlToBe("https://192.168.23.128/"));
+	}
+}
