e1116a79b8a22248b472f4177635f33b8342a954
Tomo8717 <Steph.Valm@gmail.com>
2023-10-26 11:58:52 +0200

    Addition 1 finished test SystemAdministrationPageElementsCheck
    feat: added missing tests for the rest of the unique interactable elements on the page

--- opnsenseTests/src/test/java/tests/SystemAdministrationPageElementsCheck.java
+++ opnsenseTests/src/test/java/tests/SystemAdministrationPageElementsCheck.java
@@ -50,22 +50,39 @@ public class SystemAdministrationPageElementsCheck extends LoginAction {
 	
 	@Test
 	public void consoleCheck() {
-		
+		driver.get(url+"system_advanced_admin.php");
+		//check if main interactable elements of the Console section of the page do exist
+		Assert.assertEquals(driver.findElements(By.name("usevirtualterminal")).isEmpty(), false);
+		Assert.assertEquals(driver.findElements(By.name("primaryconsole")).isEmpty(), false);
+		Assert.assertEquals(driver.findElements(By.name("secondaryconsole")).isEmpty(), false);
+		Assert.assertEquals(driver.findElements(By.name("serialspeed")).isEmpty(), false);
+		Assert.assertEquals(driver.findElements(By.name("serialusb")).isEmpty(), false);
+		Assert.assertEquals(driver.findElements(By.name("disableconsolemenu")).isEmpty(), false);
 	}
 	
 	@Test
 	public void shelleCheck() {
-		
+		driver.get(url+"system_advanced_admin.php");
+		//check if main interactable element (which is the one and only text field for timeout) of the Shell section of the page do exist
+		Assert.assertEquals(driver.findElements(By.name("autologout")).isEmpty(), false);
 	}
 	
 	@Test
 	public void authenticationCheck() {
-		
+		driver.get(url+"system_advanced_admin.php");
+		//check if main interactable elements of the Authentication section of the page do exist
+		Assert.assertEquals(driver.findElements(By.name("authmode[]")).isEmpty(), false);
+		Assert.assertEquals(driver.findElements(By.name("disableintegratedauth")).isEmpty(), false);
+		Assert.assertEquals(driver.findElements(By.name("sudo_allow_wheel")).isEmpty(), false);
+		Assert.assertEquals(driver.findElements(By.name("sudo_allow_group")).isEmpty(), false);
+		Assert.assertEquals(driver.findElements(By.name("user_allow_gen_token[]")).isEmpty(), false);
 	}
 	
 	@Test
 	public void deploymentCheck() {
-		
+		driver.get(url+"system_advanced_admin.php");
+		//check if main interactable element (which is, again, the one and only element - dropdown select) of the Deployment section of the page do exist
+		Assert.assertEquals(driver.findElements(By.name("deployment")).isEmpty(), false);
 	}
 
 }
