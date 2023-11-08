4a736ec417b2031ee8d74cd2972ee333bf468b2c
Tomo8717 <Steph.Valm@gmail.com>
2023-11-08 16:24:40 +0100

    Fix: changes in Maven dependencies.
    also added cache clear in DriverSetupAndQuit

--- opnsenseTests/src/test/java/common/DriverSetupAndQuit.java
+++ opnsenseTests/src/test/java/common/DriverSetupAndQuit.java
@@ -18,6 +18,8 @@ public static WebDriver driver = null;
 	
 	@BeforeSuite
 	public void launchBrowser () {
+		//WebDriverManager.chromedriver().clearDriverCache().setup();
+		//WebDriverManager.chromedriver().clearResolutionCache().setup();
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();	
 	}
